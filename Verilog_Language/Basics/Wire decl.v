6bf732b5a86d631c693a318d1bee0ed9deee9e9e
faintwizard <faintwizard@outlook.com>
2026-09-18 15:20:40 +0800

    Karnaugh Map

---
+++ Verilog_Language/Basics/Wire decl.v
@@ -0,0 +1,19 @@
+//https://hdlbits.01xz.net/wiki/Wire_decl
+
+`default_nettype none
+module top_module(
+    input a,
+    input b,
+    input c,
+    input d,
+    output out,
+    output out_n   ); 
+
+
+    wire a1,a2;
+    assign a1 = a&b;
+    assign a2 = c&d;
+    assign out = a1 | a2;
+    assign out_n = ~(a1 | a2);
+
+endmodule
